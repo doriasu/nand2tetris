fa98860b849b4a63570be378ab7d27126b47dd08
Takuro Omori <omori.takuro@unipro.co.jp>
2019-04-23 01:00:08 +0900

    写経

--- asm/mult.asm
+++ asm/mult.asm
@@ -7,37 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-@R2
-M=0
-@R0
-D=A
-@R2
-M=M+D
-@R1
-M=A
 
 
 (LOOP)
-@R1
-D=M
-@END
-D;JEQ
-@R1
-D=A
-
 @R2
-M=M+D
-@R1
-M=M-1
+M=0
+@i
+M=1
+
+
 @LOOP
-0;JMP
-
-
-
-
-
-
-
+@i
+D=M
+@R1
+D=D-M
+@END
+D;JGT
+@R0
+D=M
+@R2
+M=D+M
+@i
+M=M+1
+@LOOP
+0,JMP
 (END)
 @END
 0;JMP
